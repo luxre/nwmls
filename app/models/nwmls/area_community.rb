ed81f62fd4b38f6338ce4d7b1dc82302eacce62d
Bob Dutcheshen <bobdutch@gmail.com>
2013-09-27 14:43:58 -0700

    raw attributes for AreaCommunity

--- app/models/nwmls/area_community.rb
+++ app/models/nwmls/area_community.rb
@@ -1,7 +1,11 @@
 class Nwmls::AreaCommunity
   include Nwmls::Model
 
-  attr_accessor :area, :community
+  if expand_attributes?
+    attr_accessor :area, :community
+  else
+    attr_accessor :Area, :Community
+  end
 
   def self.all
     @@all ||= build_collection(Evernet::Connection.retrieve_area_community_data)
