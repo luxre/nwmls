5c3b11b28ae2a6cbf97669955bc6c77e78abdcb9
Bob Dutcheshen <bobdutch@gmail.com>
2013-09-27 16:03:56 -0700

    add raw attribute mode attars for Nwmls::Office

--- app/models/nwmls/office.rb
+++ app/models/nwmls/office.rb
@@ -1,7 +1,18 @@
 class Nwmls::Office
   include Nwmls::Model
 
-  attr_accessor :office_mlsid, :office_name, :street_care_of, :street_address, :street_city, :street_state, :street_zip_code, :street_zip_plus4, :street_county, :office_area_code, :office_phone, :fax_area_code, :fax_phone, :e_mail_address, :web_page_address, :office_type
+  def self.attribute_names
+    attrs = %w[ OfficeMLSID OfficeName StreetCareOf StreetAddress StreetCity
+      StreetState StreetZipCode StreetZipPlus4 StreetCounty OfficeAreaCode
+      OfficePhone FaxAreaCode FaxPhone EMailAddress WebPageAddress OfficeType
+    ]
+    if expand_attributes?
+      attrs = attrs.collect { |attr| attr.underscore }
+    end
+    attrs.collect { |attr| attr.to_sym }
+  end
+
+  attr_accessor(*attribute_names)
 
   def self.find(conditions = {})
     unless conditions.is_a?(Hash)
@@ -31,6 +42,14 @@ class Nwmls::Office
 
   def listings
     @listings ||= Nwmls::Listing::TYPE_TO_CLASS_MAP.collect { |type, klass| public_send(klass.demodulize.underscore.pluralize) }.sum
+  end
+
+  private
+
+  unless expand_attributes?
+    def office_mlsid
+      self.OfficeMLSID
+    end
   end
 
 end
