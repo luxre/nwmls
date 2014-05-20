d3c2b8e080f2f333adccc7041b5d9992ef9a7722
Bob Dutcheshen <bobdutch@gmail.com>
2014-05-20 16:39:12 -0700

    Fixed expanded attributes mode for amenities

--- app/models/nwmls/amenity.rb
+++ app/models/nwmls/amenity.rb
@@ -22,15 +22,14 @@ class Nwmls::Amenity
     xml.root.children.each do |amenity|
       if amenity.name == 'Amenity'
         code = amenity.at('Code').inner_text
+        keys = ['Code', 'Description', 'Values']
+        keys.collect! { |k| k.downcase } if self.expand_attributes?
         raw[code] ||= {}
-        raw[code]['Code'] = code
-        raw[code]['Description'] ||= amenity.at('Description').inner_text
-        raw[code]['Values'] ||= {}
-        raw[code]['Values'][amenity.at('Values Code').inner_text] = amenity.at('Values Description').inner_text
+        raw[code][keys.first] = code
+        raw[code][keys.second] ||= amenity.at('Description').inner_text
+        raw[code][keys.third] ||= {}
+        raw[code][keys.third][amenity.at('Values Code').inner_text] = amenity.at('Values Description').inner_text
       end
-    end
-    if self.expand_attributes?
-      raw = Hash[raw.collect { |k,v| [k.downcase, v] } ]
     end
     raw.each do |key,values|
       collection << self.new(values)
